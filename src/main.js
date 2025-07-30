057de458e7b555356dd821df2d6e7b8992e5dbac
janani <dev11@onimtait.com>
2025-07-30 16:02:24 +0530

    feat: integrate Vue Router and Tailwind CSS
    - Added vue-router as a dependency and set up basic routing with a Home component.
    - Updated App.vue to use <router-view> for rendering routed components.
    - Removed main.css and replaced it with style.css to include Tailwind CSS and custom fonts.
    - Configured PostCSS with Tailwind CSS and Autoprefixer.
    - Created initial Home.vue and index.vue pages for routing.
    - Set up Tailwind CSS configuration in tailwind.config.js.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import router from './router';
 
-import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+router.beforeEach((to, from) => {
+
+});
+
+createApp(App)
+    .use(router)
+    .mount('#app');
